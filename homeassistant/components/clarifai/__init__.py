4476032947f5553d44d0a3917bc37766986ac3b2
Francesco Rubbo <francesco.rubbo@clarifai.com>
2020-03-03 21:04:23 -0800

    support multiple models, both detection and classification

--- homeassistant/components/clarifai/__init__.py
+++ homeassistant/components/clarifai/__init__.py
@@ -1,36 +1,6 @@
 """The Clarifai integration."""
-from clarifai.rest import ClarifaiApp
-import voluptuous as vol
-
-from homeassistant.const import CONF_API_KEY
-import homeassistant.helpers.config_validation as cv
-
-DOMAIN = "clarifai"
-CONF_MODEL_NAME = "model_name"
-
-DATA_CLARIFAI_MODEL = "clarifai_model"
-
-CONFIG_SCHEMA = vol.Schema(
-    {
-        DOMAIN: vol.Schema(
-            {
-                vol.Required(CONF_API_KEY): cv.string,
-                vol.Optional(CONF_MODEL_NAME, default="general"): cv.string,
-            }
-        )
-    },
-    extra=vol.ALLOW_EXTRA,
-)
 
 
 async def async_setup(hass, config):
     """Set up the Clarifai component."""
-    if DOMAIN not in config:
-        return True
-
-    client = ClarifaiApp(api_key=config[DOMAIN].get(CONF_API_KEY))
-    model = client.models.get(config[DOMAIN].get(CONF_MODEL_NAME))
-
-    hass.data[DATA_CLARIFAI_MODEL] = model
-
     return True
--- homeassistant/components/clarifai/image_processing.py
+++ homeassistant/components/clarifai/image_processing.py
@@ -1,23 +1,39 @@
 """Image processing with Clarifai model."""
-from homeassistant.components.clarifai import DATA_CLARIFAI_MODEL
+from clarifai.rest import ClarifaiApp
+import voluptuous as vol
+
 from homeassistant.components.image_processing import (
     CONF_ENTITY_ID,
-    CONF_NAME,
     CONF_SOURCE,
+    PLATFORM_SCHEMA,
     ImageProcessingEntity,
 )
+from homeassistant.const import CONF_API_KEY
 from homeassistant.core import split_entity_id
+import homeassistant.helpers.config_validation as cv
+
+DOMAIN = "clarifai"
+CONF_MODEL_NAME = "model_name"
+
+PLATFORM_SCHEMA = PLATFORM_SCHEMA.extend(
+    {
+        vol.Required(CONF_API_KEY): cv.string,
+        vol.Optional(CONF_MODEL_NAME, default="general"): cv.string,
+    }
+)
 
 
 async def async_setup_platform(hass, config, async_add_entities, discovery_info=None):
     """Set up the Clarifai model platform."""
-    model = hass.data[DATA_CLARIFAI_MODEL]
+
+    client = ClarifaiApp(api_key=config[CONF_API_KEY])
+    model = client.models.get(config[CONF_MODEL_NAME])
 
     entities = []
     for camera in config[CONF_SOURCE]:
         entities.append(
             ClarifaiClassificationEntity(
-                camera[CONF_ENTITY_ID], model, camera.get(CONF_NAME)
+                camera[CONF_ENTITY_ID], model, config[CONF_MODEL_NAME]
             )
         )
 
@@ -35,10 +51,9 @@ class ClarifaiClassificationEntity(ImageProcessingEntity):
         self._camera = camera_entity
         self._state = None
 
-        if name:
-            self._name = name
-        else:
-            self._name = "Clarifai {0}".format(split_entity_id(camera_entity)[1])
+        self._name = "Clarifai {}, camera {}".format(
+            name, split_entity_id(camera_entity)[1]
+        )
 
     @property
     def camera_entity(self):
@@ -57,13 +72,21 @@ class ClarifaiClassificationEntity(ImageProcessingEntity):
 
     async def async_process_image(self, image):
         """Process image."""
-        response = self._model.predict_by_bytes(
-            bytearray(image), min_value=0.5, max_concepts=5
-        )
+        response = self._model.predict_by_bytes(bytearray(image))
         predictions = {}
-        results = response["outputs"][0]["data"].get("concepts", [])
-        for concept in results:
-            predictions[concept["name"]] = concept["value"]
+        if "concepts" in response["outputs"][0]["data"].keys():  # classifier
+            results = response["outputs"][0]["data"]["concepts"]
+            for concept in results[:5]:
+                predictions[concept["name"]] = round(concept["value"], 2)
+        elif "regions" in response["outputs"][0]["data"].keys():  # detector
+            results = response["outputs"][0]["data"]["regions"]
+            for ii, region in enumerate(results):
+                predictions["detection {}".format(ii)] = region["data"]["concepts"][0][
+                    "name"
+                ]
+        else:
+            pass
+
         self._predictions = predictions
         self._state = 1
 
